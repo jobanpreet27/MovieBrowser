616d653c13bdae094d3955fa373b5c2aed66a197
jobanpreet27 <57243235+jobanpreet27@users.noreply.github.com>
2020-07-19 01:07:02 -0500

    font changed

--- screens/DetailPage.js
+++ screens/DetailPage.js
@@ -4,7 +4,7 @@ import { Text, View, StyleSheet,Button,TextInput  } from 'react-native';
 export default class DetailPage extends React.Component{
    render(){
       return(<View>
-       <Text style = {{flex:1,justifyContent:'center'}}>
+       <Text style = {{flex:1,justifyContent:'center',fontSize:20,}}>
           Development in Progress 
        </Text> 
        
